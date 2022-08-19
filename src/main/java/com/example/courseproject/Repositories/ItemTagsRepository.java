0ae262213f7d1b4396901c0ebdb84b19e08ac026
Abbos <yoldoshev-1994@mail.ru>
2022-08-19 21:23:16 +0500

    item settings page UI changed

--- src/main/java/com/example/courseproject/Repositories/ItemTagsRepository.java
+++ src/main/java/com/example/courseproject/Repositories/ItemTagsRepository.java
@@ -1,4 +1,4 @@
-package com.example.courseproject.Controllers;
+package com.example.courseproject.Repositories;
 
 import com.example.courseproject.model.ItemTags;
 import org.springframework.data.jpa.repository.JpaRepository;
